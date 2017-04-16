96d5f9459f7c182f766133764f35272f1ec03f03
memochka1 <stefurin.marina@gmail.com>
2017-04-16 16:47:54 -0400

    added required commits

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,27 @@
+//Maryna Burge
+/*
+Создать консольное приложение, которое просит пользователя ввести из консоли 5 чисел
+Все числа должны быть записаны в массив целых чисел.
+
+После этого, массив должен быть отсортирован по возрастанию и выведен консоль.
+Сначала начальная версия, далее - отсортированная.
+
+Пример:
+
+Enter number 1:
+5
+Enter number 2:
+4
+Enter number 3:
+3
+Enter number 4:
+2
+Enter number 5:
+1
+
+Initial array: 5, 4, 3, 2, 1
+Sorted array: 1, 2, 3, 4, 5.
+ */
 package com.company;
 import java.util.Arrays;
 import java.util.Scanner;
