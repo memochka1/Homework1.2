ce53ca3201ccdd676a4259e29a96d18f135fb238
memochka1 <stefurin.marina@gmail.com>
2017-04-17 17:23:27 -0400

    Changed to add bubble sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 //Maryna Burge
+//Перероблено з функцією Bubble
 /*
 Создать консольное приложение, которое просит пользователя ввести из консоли 5 чисел
 Все числа должны быть записаны в массив целых чисел.
@@ -23,11 +24,10 @@ Initial array: 5, 4, 3, 2, 1
 Sorted array: 1, 2, 3, 4, 5.
  */
 package com.company;
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 import static java.lang.Integer.parseInt;
-import static java.util.Arrays.*;
 
 public class Main {
 
@@ -47,11 +47,32 @@ public class Main {
             outString1+= numArray[x]+(x>=numArray.length-1?"":",");
         }
         System.out.println(outString1);
-      sort(numArray);
+        numArray=SortAscending(numArray);
         String outString2="Sorted array: ";
         for(int x=0;x<numArray.length;x++){
             outString2+= numArray[x]+(x>=numArray.length-1?"":",");
         }
         System.out.println(outString2+".");
     }
+    public static int[] SortAscending(int[] sortArray){
+
+        int j;
+        boolean complete = true;
+        int num;
+
+        while (complete) {
+            complete = false;
+            for (j = 0; j < sortArray.length - 1; j++) {
+                if (sortArray[j] > sortArray[j + 1]) {
+                    num = sortArray[j];
+                    sortArray[j] = sortArray[j + 1];
+                    sortArray[j + 1] = num;
+                    complete = true;
+                }
+            }
+
+        }
+        return sortArray;
+    }
+
 }
